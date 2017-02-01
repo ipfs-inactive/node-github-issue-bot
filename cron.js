3e77d18e9e72e368ca7e0cdaf9cc05c168e5978c
Richard Littauer <richard.littauer@gmail.com>
2017-02-01 13:34:01 +0000

    Fix sprintbot

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@ const createIssue = require('./')
 const cron = require('node-cron')
 const helpers = require('./helpers')
 
-cron.schedule('* 12 * * 3', function () {
+cron.schedule('0 12 * * 3', function () {
   createIssue({
     title: `Monday Calls: ${helpers.getTitleFormat(8, 14)}`,
     template: 'templates/ipfs-all-hands.md',
